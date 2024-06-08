f4cb45df9ebb2d33c176af75daf83226d9d6e86c
Konalawebsites <atfitcad@gmail.com>
2024-06-08 13:56:02 +0300

    redy

--- src/comps/BottomBar.jsx
+++ src/comps/BottomBar.jsx
@@ -2,26 +2,31 @@ import React from "react"
 import { styled } from "styled-components"
 import { Box, Button, Layer, Text } from "grommet"
 import { StyledBottomBar } from "../../styles/styles"
-import { Emoji } from 'grommet-icons'
+import { MailOption } from 'grommet-icons'
 import ContactForm from './ContactForm.jsx'
 
 const CustomText = styled(Text)`
   font-family: 'Unispace';
   font-size: 24px;
-  font-weight: bold;
 `;
 
+const CustomText2 = styled(Text)`
+  font-family: 'Unispace';
+  font-size: 12px;
+  height: 15px;
+`;
 const BottomBar = () => {
     const [open, setOpen] = React.useState(false);
     return (
         <StyledBottomBar>
             <Box direction="row" justify="between" width="100%">
-                <Box align="center" justify="center" margin={{ left: "15px" }}>
+                <Box align="center" justify="center" margin={{ left: "15px" }} direction="column" gap="1px">
                     <CustomText> my photos </CustomText>
+                    <CustomText2 margin={{left: "55px"}}> Aleksi Tuominen </CustomText2>
                 </Box>
 
                 <Box align="center" pad="small">
-                    <Button onClick={() => setOpen(!open)} icon={<Emoji />} />
+                    <Button onClick={() => setOpen(!open)} icon={<MailOption />} />
                     {open && (
                         <Layer
                         style={{ position: 'relative', top: '30%', width: "50%" }}
